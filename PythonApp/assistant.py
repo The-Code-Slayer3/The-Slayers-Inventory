09ce610fff0396f678a213988edbbff251e68786
The-Code-Slayer3 <122614779+The-Code-Slayer3@users.noreply.github.com>
2023-01-15 15:47:47 +0530

    Update assistant.py

--- PythonApp/assistant.py
+++ PythonApp/assistant.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from subprocess import call
 voice_processor = pyttsx3.init('sapi5')
 
-ch_voice=int(input("for Male Voice Enter 1, for Female voice enter 2\n"))
+global ch_voice=int(input("for Male Voice Enter 1, for Female voice enter 2\n"))
 match ch_voice:
     case 1:
         # voice[0].id ie male
@@ -45,16 +45,16 @@ def greet():
 def Userinput():
     r = sr.Recognizer()
     with sr.Microphone() as source:
+        r.pause_threshold=1.125 # rate of speaking of the user, sr= amt of words/time taken
         print("listening....")
-        r.pause_threshold = 1.125 # rate of speaking of the user, sr= amt of words/time taken
         audio = r.listen(source)
         r.adjust_for_ambient_noise(source)
     try:
         print('recognising....')
-        query = r.recognize_google(audio, language='en-in')
-        print(f"User said: {query}\n")
+        query = r.recognize_google(audio, language='en-in') # you can use em_IN, en_US,en_CA for indian, american, canadian english, but make it sure that your voice model supports it
+        print(f"You Said: {query}\n")
     except Exception:
-        print("Say that again please...")   #Say that again will be printed in case of improper voice 
+        print("Say that again please...")   #Say that again will be printed if errors in voice czannot be cleared 
         return "None"
     return query
 if __name__ == '__main__':
@@ -67,7 +67,7 @@ if __name__ == '__main__':
 
         if "change voice" in query:
             speak("Okay, choose your voice....., enter 1 for male 2 for female")
-            ch_voice=int(input())
+            #ch_voice=int(input()) # local variable Defaukt is global ch_voice variable
             match ch_voice:
                 case 1:
                     voice_processor.setProperty('voice', voice_processor.getProperty('voices')[0].id)
@@ -82,7 +82,7 @@ if __name__ == '__main__':
             call("pip install --upgrade --no-cache-dir " + ' '.join([dist.project_name for dist in pkg_resources.working_set]), shell=True)
             msvcrt.getch()
 
-        if "wait" in query:
+        if "wait" in query: # waits until you press Enter 
             speak("Ok, Press Enter When You want to call me")
             msvcrt.getch()
 
@@ -101,7 +101,6 @@ if __name__ == '__main__':
             msvcrt.getch()
 
         if "google" in query:
-            query.replace("google", "")
             pykt.search(query)
             speak("I'll be waiting in Background, press enter when finished to procced..")
             print("Press Enter to Continue....")
